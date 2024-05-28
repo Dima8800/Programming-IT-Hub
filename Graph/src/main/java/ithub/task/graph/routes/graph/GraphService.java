4a41d0a5938186e70b85031db047e33e141a806e
i22s0623 <i22s0623@students.it-college.ru>
2024-05-28 19:27:22 +0500

    graph

--- Graph/src/main/java/ithub/task/graph/routes/graph/GraphService.java
+++ Graph/src/main/java/ithub/task/graph/routes/graph/GraphService.java
@@ -13,9 +13,7 @@ public class GraphService {
 
     public String GenerateSVGByStringForGraph(String request){
         List<Edge> edges = parseGraph(request);
-        String svg = buildGraphSVG(edges);
-        System.out.println(svg);
-        return svg;
+        return buildGraphSVG(edges);
     }
 
     public String NextStep(Integer step){
@@ -28,14 +26,29 @@ public class GraphService {
 
     public String buildGraphSVG(List<Edge> edges) {
         StringBuilder svg = new StringBuilder();
-        svg.append("<svg width=\"500\" height=\"500\">");
+        svg.append("<svg width=\"550\" height=\"550\">");
+
+        svg.append("<defs>\n" +
+                "    <marker\n" +
+                "      id=\"arrow\"\n" +
+                "      viewBox=\"0 0 10 10\"\n" +
+                "      refX=\"15\"\n" +
+                "      refY=\"5\"\n" +
+                "      markerWidth=\"8\"\n" +
+                "      markerHeight=\"8\"\n" +
+                "fill=\"black\""+
+                "      orient=\"auto-start-reverse\">\n" +
+                "      <path d=\"M 0 0 L 10 5 L 0 10 z\" />\n" +
+                "    </marker>\n" +
+                "  </defs>");
 
         for (Edge edge : edges) {
-            svg.append("<line x1=\"" + edge.getStart().getX() + "\" y1=\"" + edge.getStart().getY() + "\" x2=\"" + edge.getEnd().getX() + "\" y2=\"" + edge.getEnd().getY() + "\" stroke=\"black\" />");
+            svg.append("<line x1=\"" + edge.getStart().getX() + "\" y1=\"" + edge.getStart().getY() + "\" x2=\"" + edge.getEnd().getX() + "\" y2=\""
+                    + edge.getEnd().getY() + "\" stroke=\"black\"" + "marker-end=\"url(#arrow)\"" + "/>");
 
-            int textX = (edge.getStart().getX() + edge.getEnd().getX()) / 2; // Вычисляем середину по X
-            int textY = (edge.getStart().getY() + edge.getEnd().getY()) / 2; // Вычисляем середину по Y
-            svg.append("<text x=\"" + textX + "\" y=\"" + textY + "\" fill=\"green\">" + ">" + edge.getWeight() + "</text>");
+            int textX = (edge.getStart().getX() + edge.getEnd().getX()) / 2;
+            int textY = (edge.getStart().getY() + edge.getEnd().getY()) / 2;
+            svg.append("<text x=\"" + textX + "\" y=\"" + textY + "\" fill=\"green\">" + edge.getWeight() + "</text>");
         }
 
         Set<String> key = coordinate.keySet();
@@ -55,9 +68,10 @@ public class GraphService {
             input = input.replaceAll("\\{\"data\":\"", "")
                     .replaceAll("}", "")
                     .replaceAll("\"","");
-            List<Edge> edges = new ArrayList<>();
 
+            List<Edge> edges = new ArrayList<>();
             String bycoordinate = input.split("\\|")[0];
+
             for (int i = 0 ; i < bycoordinate.split(" ").length; i++){
                 String name = bycoordinate.split(" ")[i];
                 coordinate.put(name, new coordinates(name, random.nextInt(450), random.nextInt(450)));
@@ -67,15 +81,10 @@ public class GraphService {
                 coordinates start = coordinate.get(input.split("\\|")[i].split(" ")[0]);
                 coordinates end = coordinate.get(input.split("\\|")[i].split(" ")[1]);
                 edges.add(new Edge(
-                        new coordinates(
-                                start.getName(),
-                                start.getX(),
-                                start.getY()),
-                        new coordinates(
-                                end.getName(),
-                                end.getX(),
-                                end.getY()),
-                        Integer.parseInt(input.split("\\|")[i].split(" ")[2])));
+                        start,
+                        end,
+                        Integer.parseInt(input.split("\\|")[i].split(" ")[2]))
+                );
             }
             return edges;
         }catch (Exception err){
