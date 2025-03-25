efbb915772d1eb6377d6240caa8c2fbacfb9ceeb
Dima8800 <dmitry2006happy@gmail.com>
2025-03-25 13:01:49 +0500

    init

---
+++ Task/graphql/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.4.4"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "it-hub.task"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-graphql")
+	
+	// ModelMapper
+	implementation("org.modelmapper:modelmapper:3.2.0")
+
+	implementation("org.springframework.boot:spring-boot-starter-web")
+
+	// Lombok
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.graphql:spring-graphql-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ Task/graphql/src/test/java/it_hub/task/graphql/GraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package it_hub.task.graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
